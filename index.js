c456acb4a7bbcb02b26dc0bae6a55b572dbbfcec
goyalaadesh461@gmail.com <goyalaadesh462@gmail.com>
2021-01-21 23:15:03 +0530

    first commit:

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+const authRoutes = require('./routes/auth-routes');
+const orderRoutes = require('./routes/order-routes');
+const shopRoutes = require('./routes/shop-routes');
+
+mongoose.connect('mongodb+srv://goyalaadesh461:11710461Aa@test.d3lnu.mongodb.net/mega_project?retryWrites=true&w=majority',{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Database Connected');
+}).catch((err) => {
+    console.log('Error', err);
+    process.exit(1);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(authRoutes);
+app.use(orderRoutes);
+app.use(shopRoutes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,function() {
+    console.log('Server listening on port ' + port); 
+});
---
+++ models/menu.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const menuSchema = new mongoose.Schema({
+    item : {
+        type : String,
+        required : true
+    },
+    price: {
+        type : String,
+        required : true
+    },
+    quantity: {
+        type : Number,
+        required : true
+    },
+    shop : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'Shop',
+        required : true
+    }
+});
+
+module.exports = mongoose.model('Menu',menuSchema);
