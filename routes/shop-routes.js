1af9cad0f2c586dec1de540e64bda0ba974d452a
goyalaadesh461@gmail.com <goyalaadesh462@gmail.com>
2021-02-08 21:49:07 +0530

    shop items

--- routes/shop-routes.js
+++ routes/shop-routes.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const requireAuth = require('../middlewares/require-auth');
+const Menu = require('../models/menu');
+const Shop = require('../models/shop');
 
-router.get('/signin',function (req, res){
-    res.send({message: 'Helpers'});
+router.post('/api/shop/addproduct',requireAuth, async (req, res) => {
+    const {item, price, quantity} = req.body;
+    const newMenu = new Menu({
+        item: item,
+        price: price,
+        quantity: quantity,
+        shop: req.currentShop._id
+    });
+    await newMenu.save();
+    return res.status(200).send({message: 'Item added successfully'});
+});
+
+router.get('/api/shop/:id', async function(req, res){
+    const menu = await Menu.find({shop: req.params.id, quantity: {$gt: 0}});
+    return res.status(200).send({message: 'Item in shop' , menu});
 });
 
 module.exports = router;
